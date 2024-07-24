dcbd8e02fb3204a3957f8b34d66112bfcc6b0123
Daniel <danoladesign@gmail.com>
2024-07-24 17:14:44 -0400

    SF000: Initial project setup and configuration
    - Installed dependencies:
      - @types/node: ^20.14.12
      - lite-server: ^2.6.1
      - typescript: ^5.5.4
    - Set up development dependencies:
      - lite-server: ^2.6.1
      - nodemon: ^3.1.4
      - concurrently: ^7.0.0
    - Configured tsconfig.json
    - Configured nodemon.js
    - Configured lite-server bs-config.json
    - Added favicon
    - Created src/index.html
    - Created src/main.ts
    - Added DOMContentLoaded event listener to append canvas to the body

---
+++ dist/main.js
@@ -0,0 +1,6 @@
+"use strict";
+document.addEventListener('DOMContentLoaded', () => {
+    const canvas = document.createElement('canvas');
+    document.body.appendChild(canvas);
+    console.log("appended canvas");
+});
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const canvas = document.createElement('canvas');
+    document.body.appendChild(canvas);
+    console.log("appended canvas")
+});
