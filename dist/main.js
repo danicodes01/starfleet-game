a611dc165b9dfd5f71abd55b8926bbbcc1dec54f
Daniel <danoladesign@gmail.com>
2024-07-24 18:09:25 -0400

    SF002
    - Setup levels class
    - Created Star entity with draw and update methods
    - Integrated Starfield class with dynamic canvas resizing and star initialization
    - Added game loop in Starfield class for updating and drawing stars
    - Initialized and started game in main.ts

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,9 @@
-"use strict";
+import { Game } from './game.js';
 document.addEventListener('DOMContentLoaded', () => {
     const canvas = document.createElement('canvas');
     document.body.appendChild(canvas);
     console.log("appended canvas");
+    const game = new Game(canvas);
+    console.log("starting game");
+    game.start();
 });
--- src/starfield.ts
+++ src/starfield.ts
@@ -1,24 +1,80 @@
+import { Star } from "./entities/star.js";
+import { Drawable } from "./utils/drawable.js";
+import {levels, Level} from "./utils/levels.js";
+
 export class Starfield {
-    canvas: HTMLCanvasElement;
-    maxDepth: number;
-    
-    constructor(canvas: HTMLCanvasElement) {
-        this.canvas = canvas
+  canvas: HTMLCanvasElement;
+  maxDepth: number;
+  ctx: CanvasRenderingContext2D;
+  stars: Star[] = [];
+  currentLevel: number;
+  
 
-        this.maxDepth = Math.max(this.canvas.width, this.canvas.height) * 1.5;
+  constructor(canvas: HTMLCanvasElement) {
+    this.canvas = canvas;
+    this.ctx = canvas.getContext("2d")!;
+    this.currentLevel = 0;
 
-        this.resizeCanvas();
-        window.addEventListener("resize", this.resizeCanvas.bind(this));
+    this.maxDepth = Math.max(this.canvas.width, this.canvas.height) * 1.5;
+
+    this.resizeCanvas();
+    window.addEventListener("resize", this.resizeCanvas.bind(this));
+    this.intitStars();
+}
+
+  resizeCanvas() {
+    const width = window.innerWidth * 0.85;
+    const height = window.innerHeight * 0.85;
+    this.canvas.width = width;
+    this.canvas.height = height;
+    this.canvas.style.position = "absolute";
+    this.canvas.style.left = `${(window.innerWidth - width) / 2}px`;
+    this.canvas.style.top = `${(window.innerHeight - height) / 2}px`;
+    this.maxDepth = Math.max(this.canvas.width, this.canvas.height) * 1.5;
+    console.log(this.maxDepth, "hello");
+  }
+
+  get currentLevelConfig(): Level {
+    return levels[this.currentLevel];
+  }
+
+
+  intitStars() {
+    this.stars = [];
+    const levelConfig = this.currentLevelConfig;
+    for (let i = 0; i < levelConfig.starNum; i++) {
+      this.stars.push(new Star(this.canvas.width, this.canvas.height, this.maxDepth));
     }
+  }
 
-    resizeCanvas() {
-        const width = window.innerWidth * 0.85;
-        const height = window.innerHeight * 0.85;
-        this.canvas.width = width;
-        this.canvas.height = height;
-        this.canvas.style.position = 'absolute';
-        this.canvas.style.left = `${(window.innerWidth - width) / 2}px`;
-        this.canvas.style.top = `${(window.innerHeight - height) / 2}px`;
-        this.maxDepth = Math.max(this.canvas.width, this.canvas.height) * 1.5;
+  updateStars() {
+    this.stars = this.stars.filter(star => {
+       if (star instanceof Star) {
+        star.update(this.canvas.width, this.canvas.height, this.maxDepth, this.currentLevelConfig.starSpeed);
       }
+      return true;
+    });
+  }
+
+  drawStars() {
+    const levelConfig = this.currentLevelConfig;
+    for (let star of this.stars) {
+        star.draw(this.ctx, this.canvas.width, this.canvas.height, levelConfig.starSize, levelConfig.starColor);
+    }
+  }
+  
+  loop(timeNow: number) {
+    const levelConfig = this.currentLevelConfig;
+    this.ctx.fillStyle = levelConfig.spaceColor;
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+
+    this.updateStars();
+    this.drawStars();
+
+    requestAnimationFrame(this.loop.bind(this));
+  }
+
+  start() {
+    requestAnimationFrame(this.loop.bind(this));
+  }
 }
