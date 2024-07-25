e1688b7bb4c40ab3bd460e02f325a739ef2f62ee
Daniel <danoladesign@gmail.com>
2024-07-25 11:15:59 -0400

    SF012: Added touch event listeners

--- src/inputHandler.ts
+++ src/inputHandler.ts
@@ -12,5 +12,24 @@ export class InputHandler {
     document.addEventListener("keyup", (e) => {
       starfield.keysPressed[e.key] = false;
     });
+
+    document.addEventListener("touchstart", (e) => {
+      e.preventDefault();
+      const touch = e.touches[0];
+      starfield.crosshair.x = touch.clientX;
+      starfield.crosshair.y = touch.clientY;
+      starfield.shoot();
+    });
+
+    document.addEventListener("touchmove", (e) => {
+      e.preventDefault();
+      const touch = e.touches[0];
+      starfield.crosshair.x = touch.clientX;
+      starfield.crosshair.y = touch.clientY;
+    });
+
+    document.addEventListener("touchend", (e) => {
+      e.preventDefault();
+    });
   }
 }
